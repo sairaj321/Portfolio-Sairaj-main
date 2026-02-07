9b90e09943a9e9cd579effa0092a74f7ed73be47
sairaj kadlag <sairajkadlag53@gmail.com>
2026-02-07 14:29:20 +0530

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/13RXhUeJ2HraSwXecMYt9QSKTUUYrK3sy/view?usp=drive_link"
+            href="https://drive.google.com/file/d/15WE_t_xeIkw7sdee4Mj4oJWTel9RexwO/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
